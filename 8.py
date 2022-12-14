a7480f53bad9e23e543939023d2aa8c273275b40
Love Löfdahl <love.lofdahl@gmail.com>
2022-12-14 23:18:53 +0100

    Day 8
    Provide solution for day 8.

--- 8.py
+++ 8.py
@@ -3,15 +3,32 @@ from util.util import transpose
 import numpy as np
 
 
-def is_visible(first, second, tree):
-    return len(first) == 0 or len(second) == 0 or \
-        (max(first, key=lambda t: t.height).height < tree.height or
-         max(second, key=lambda t: t.height).height < tree.height)
+def is_visible(before, after, tree):
+    tree_on_edge = len(before) == 0 or len(after) == 0
+    if tree_on_edge:
+        return True
+    blocked_before = max(before, key=lambda t: t.height).height < tree.height
+    blocked_after = max(after, key=lambda t: t.height).height < tree.height
+    return tree_on_edge or blocked_before or blocked_after
+
+def view_distance(before, after, tree):
+    distance_before = check_for_higher_trees(np.flip(before), tree.height)
+    distance_after = check_for_higher_trees(after, tree.height)
+    return distance_after, distance_before
+
+def check_for_higher_trees(_list, height):
+    higher_trees_before = []
+    for tree in _list:
+        higher_trees_before.append(tree)
+        if tree.height >= height:
+            return len(higher_trees_before)
+    return len(_list)
 
 class Tree:
     def __init__(self, height):
         self.height = int(height)
         self.visible = True
+        self.view_distance = []
 
     def __str__(self):
         return f'({self.height}, {self.visible})'
@@ -19,40 +36,60 @@ class Tree:
 
 class Forest:
     def __init__(self):
-        self.matrix = None
+        self.trees = None
 
     def _heights(self, row):
-        return list(map(lambda tree: tree.height, self.matrix[row]))
+        return list(map(lambda tree: tree.height, self.trees[row]))
     def add_row(self, row):
         _temp = [Tree(x) for x in row]
-        if self.matrix is None:
-            self.matrix = [_temp]
+        if self.trees is None:
+            self.trees = [_temp]
         else:
-            self.matrix = np.append(self.matrix, [_temp], axis=0)
+            self.trees = np.append(self.trees, [_temp], axis=0)
 
     def check_tree_visibility(self):
-        print(self.matrix)
-        for row in self.matrix:
+        for row in self.trees:
             for index, tree in enumerate(row):
-                first = row[0: index]
-                second = row[index + 1:]
-                tree.visible = is_visible(first, second, tree)
-        for column_number in range(len(self.matrix[0])):
-            column = self.matrix[:column_number]
-            for tree in column:
-                first = column[0: index]
-                second = column[index + 1:]
-                tree.visible = is_visible(first, second, tree)
+                before = row[0: index]
+                after = row[index + 1:]
+                tree.visible = is_visible(before, after, tree)
+        for column in self.trees.T:
+            for index, tree in enumerate(column):
+                if not tree.visible:
+                    before = column[0: index]
+                    after = column[index + 1:]
+                    visible = is_visible(before, after, tree)
+                    tree.visible = visible
+
+    def max_scenic_score(self):
+        for row in self.trees:
+            for index, tree in enumerate(row):
+                before = row[0: index]
+                after = row[index + 1:]
+                left, right = view_distance(before, after, tree)
+                tree.view_distance.extend([left, right])
+        for column in self.trees.T:
+            for index, tree in enumerate(column):
+                before = column[0: index]
+                after = column[index + 1:]
+                up, down = view_distance(before, after, tree)
+                tree.view_distance.extend([up, down])
+        max_scenic_score = 0
+        for tree in np.nditer(self.trees, flags=["refs_ok"]):
+            item = tree.item(0)
+            max_scenic_score = max(max_scenic_score, np.prod(item.view_distance))
+        return max_scenic_score
+
 
     def number_of_visible_trees(self):
         _sum = 0
-        for row in self.matrix:
+        for row in self.trees:
             _sum += len(list(filter(lambda tree: tree.visible, row)))
         return _sum
 
     def __str__(self):
         as_string = ''
-        for row in self.matrix:
+        for row in self.trees:
             for tree in row:
                 as_string += f' {tree}'
             as_string += '\n'
@@ -60,12 +97,15 @@ class Forest:
 
 def execute():
     forest = Forest()
-    with open('data/test.txt') as file:
+    with open('data/8.txt') as file:
         for line in file:
             forest.add_row(line.strip())
     forest.check_tree_visibility()
     print(forest)
-    print(forest.number_of_visible_trees())
+    print('max number of visible trees:', forest.number_of_visible_trees())
+    print('max scenic score', forest.max_scenic_score())
+
+
 
 
 if __name__ == '__main__':
